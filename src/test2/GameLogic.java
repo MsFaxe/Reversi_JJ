560eb02ef12de348e09e074919734e4df6f00938
MsFaxe <anty.wjj@gmail.com>
2020-05-12 19:16:29 +0200

    12.05.2020

--- src/test2/GameLogic.java
+++ src/test2/GameLogic.java
@@ -11,21 +11,27 @@ public class GameLogic extends BoardGameScene {
     private boolean action = true;
 
     public List<Label> unlockLabel() {
-        Map<Key, Label> labelMap = new HashMap<>();
-
-        for (int i = 0; i < 8; i++) { // i - rows
-            for (int j = 0; j < 8; j++) { //j - columns
-                labelMap.put(new Key(i, j), getOneLabel(i, j));
-            }
-        }
-
-        String color = StartGameScene.getPawnColor();
+//        Map<Key, Label> labelMap = new HashMap<>();
+//
+//        for (int i = 0; i < 8; i++) { // i - rows
+//            for (int j = 0; j < 8; j++) { //j - columns
+//                labelMap.put(new Key(i, j), getOneLabel(i, j));
+//            }
+//        }
+//
+//        String color = StartGameScene.getPawnColor();
 
         List<Label> list1 = new ArrayList<>();
 
-        for (Map.Entry<Key, Label> label : labelMap.entrySet()) {
-            list1.add(label.getValue());
+        for (int i = 0; i < 8; i++) { // i - rows
+            for (int j = 0; j < 8; j++) { //j - columns
+                list1.add(getOneLabel(i, j));
+            }
         }
+
+//        for (Map.Entry<Key, Label> label : labelMap.entrySet()) {
+//            list1.add(label.getValue());
+//        }
 
         List<Label> list2 = list1.stream()
                 .filter(e -> (e.getGraphic() != null))
@@ -53,7 +59,6 @@ public class GameLogic extends BoardGameScene {
 
 
     public void humanPlay() {
-
         List<Label> list = unlockLabel();
 
         for (Label label : list) {
@@ -72,30 +77,7 @@ public class GameLogic extends BoardGameScene {
                     getOneLabel(i, j).setId(getPawnColor());
                     getOneLabel(i, j).setPickOnBounds(false);
 
-
-                    if (i < 7 && getOneLabel(i + 1, j).getGraphic() != null /*&& getOneLabel(i + 1, j).getId() != getPawnColor()*/) {
-                        getOneLabel(i + 1, j).setGraphic(getDot(getPawnColor())); //2nd action
-                        getOneLabel(i + 1, j).setId(getPawnColor());
-                        getOneLabel(i + 1, j).setPickOnBounds(false);
-                    }
-
-                    if (i > 0 && getOneLabel(i - 1, j).getGraphic() != null /*&& getOneLabel(i - 1, j).getId() != getPawnColor()*/) {
-                        getOneLabel(i - 1, j).setGraphic(getDot(getPawnColor()));
-                        getOneLabel(i - 1, j).setId(getPawnColor());
-                        getOneLabel(i - 1, j).setPickOnBounds(false);
-                    }
-
-                    if (j < 7 && getOneLabel(i, j + 1).getGraphic() != null /*&& getOneLabel(i, j + 1).getId() != getPawnColor()*/) {
-                        getOneLabel(i, j + 1).setGraphic(getDot(getPawnColor()));
-                        getOneLabel(i, j + 1).setId(getPawnColor());
-                        getOneLabel(i, j + 1).setPickOnBounds(false);
-                    }
-
-                    if (j > 0 && getOneLabel(i, j - 1).getGraphic() != null /*&& getOneLabel(i, j - 1).getId() != getPawnColor()*/) {
-                        getOneLabel(i, j - 1).setGraphic(getDot(getPawnColor()));
-                        getOneLabel(i, j - 1).setId(getPawnColor());
-                        getOneLabel(i, j - 1).setPickOnBounds(false);
-                    }
+                    changeColorOfPowns(i, j, getPawnColor());
 
                     computerPlay(); // 3rd action
                 }
@@ -105,26 +87,74 @@ public class GameLogic extends BoardGameScene {
         //list2.stream().forEach(System.out::println);
     }
 
-    private int i = 0;
+    private int round = 0;
 
     public void computerPlay() {
 
-        System.out.println("good job: round " + i);
+        System.out.println("good job: round " + round);
 
         List<Label> list = unlockLabel();
         Random random = new Random();
 
         Label label = list.get(random.nextInt(list.size()));
+        System.out.println(label);
+
+        int i = Integer.parseInt(label.getText().substring(0, 1)); //horizontalIndex
+        int j = Integer.parseInt(label.getText().substring(1)); //verticalIndex
 
         if (getPawnColor() == "white") {
+
             label.setGraphic(getDot("black"));
             label.setPickOnBounds(false);
+
+            changeColorOfPowns(i, j, "black");
+
         }else {
             label.setGraphic(getDot("white"));
             label.setPickOnBounds(false);
+
+            changeColorOfPowns(i, j, "white");
+
         }
 
-        i++;
+        round++;
         humanPlay();
+    }
+
+    public boolean possibleToClick(int i, int j){
+        if ((i < 7 && getOneLabel(i + 1, j).getGraphic() != null && getOneLabel(i + 1, j).getId() != getPawnColor())
+                || (j > 0 && getOneLabel(i, j - 1).getGraphic() != null && getOneLabel(i, j - 1).getId() != getPawnColor())
+                || (j < 7 && getOneLabel(i, j + 1).getGraphic() != null && getOneLabel(i, j + 1).getId() != getPawnColor())
+                || (i > 0 && getOneLabel(i - 1, j).getGraphic() != null && getOneLabel(i - 1, j).getId() != getPawnColor())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void changeColorOfPowns(int i, int j, String string){
+        if (i < 7 && getOneLabel(i + 1, j).getGraphic() != null /*&& getOneLabel(i + 1, j).getId() != getPawnColor()*/) {
+            getOneLabel(i + 1, j).setGraphic(getDot(string)); //2nd action
+            getOneLabel(i + 1, j).setId(string);
+            getOneLabel(i + 1, j).setPickOnBounds(false);
+        }
+
+        if (i > 0 && getOneLabel(i - 1, j).getGraphic() != null /*&& getOneLabel(i - 1, j).getId() != getPawnColor()*/) {
+            getOneLabel(i - 1, j).setGraphic(getDot(string));
+            getOneLabel(i - 1, j).setId(string);
+            getOneLabel(i - 1, j).setPickOnBounds(false);
+        }
+
+        if (j < 7 && getOneLabel(i, j + 1).getGraphic() != null /*&& getOneLabel(i, j + 1).getId() != getPawnColor()*/) {
+            getOneLabel(i, j + 1).setGraphic(getDot(string));
+            getOneLabel(i, j + 1).setId(string);
+            getOneLabel(i, j + 1).setPickOnBounds(false);
+        }
+
+        if (j > 0 && getOneLabel(i, j - 1).getGraphic() != null /*&& getOneLabel(i, j - 1).getId() != getPawnColor()*/) {
+            getOneLabel(i, j - 1).setGraphic(getDot(string));
+            getOneLabel(i, j - 1).setId(string);
+            getOneLabel(i, j - 1).setPickOnBounds(false);
+        }
     }
 }
